c7aca91a703c871f86370aa029906d76f3992256
Adam Binnersley <abinnersley@gmail.com>
2018-02-06 15:45:25 +0000

    Add coverage to unit tests

--- tests/PaginationTest.php
+++ tests/PaginationTest.php
@@ -31,10 +31,12 @@ class PaginationTest extends TestCase{
      * @covers Pager\Pagination::getPage
      */
     public function testNoPagerNeeded(){
-        $this->markTestIncomplete('Test not yet implemented');
+        $this->assertEquals('', $this->pagination->paging(10, '/test-page'));
     }
     
     /**
+     * @covers Pager\Pagination::setPaginationClass
+     * @covers Pager\Pagination::getPaginationClass
      * @covers Pager\Pagination::paging
      * @covers Pager\Pagination::buildLink
      * @covers Pager\Pagination::getPage
@@ -54,5 +56,31 @@ class PaginationTest extends TestCase{
      */
     public function testPagerWithQueryString(){
         $this->markTestIncomplete('Test not yet implemented');
+    }
+    
+    /**
+     * @covers Pager\Pagination::setPaginationClass
+     * @covers Pager\Pagination::getPaginationClass
+     */
+    public function testSetPagerClass(){
+        $this->assertEquals('pagination', $this->pagination->getPaginationClass());
+        $this->pagination->setPaginationClass('my-class');
+        $this->assertNotEquals('pagination', $this->pagination->getPaginationClass());
+        $this->assertEquals('my-class', $this->pagination->getPaginationClass());
+        $this->pagination->setPaginationClass('paginationclass6');
+        $this->assertEquals('paginationclass6', $this->pagination->getPaginationClass());
+    }
+    
+    /**
+     * @covers Pager\Pagination::setPaginationClass
+     * @covers Pager\Pagination::getPaginationClass
+     */
+    public function testSetActiveClass(){
+        $this->assertEquals('active', $this->pagination->getPaginationClass());
+        $this->pagination->setPaginationClass('current');
+        $this->assertNotEquals('active', $this->pagination->getPaginationClass());
+        $this->assertEquals('current', $this->pagination->getPaginationClass());
+        $this->pagination->setPaginationClass('current active-class');
+        $this->assertEquals('current active-class', $this->pagination->getPaginationClass());
     }
 }
