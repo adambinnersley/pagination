232a8cbff3068128286a1814e1a243d269d3459d
Adam Binnersley <abinnersley@gmail.com>
2020-10-08 16:03:13 +0100

    Update PHPunit & tests

--- tests/PaginationTest.php
+++ tests/PaginationTest.php
@@ -4,15 +4,18 @@ namespace Pager\Tests;
 use Pager\Pagination;
 use PHPUnit\Framework\TestCase;
 
-class PaginationTest extends TestCase{
+class PaginationTest extends TestCase
+{
     
     protected $pagination;
     
-    public function setUp() {
+    public function setUp(): void
+    {
         $this->pagination = new Pagination();
     }
     
-    public function tearDown(){
+    public function tearDown(): void
+    {
         unset($this->pagination);
     }
     
@@ -26,29 +29,30 @@ class PaginationTest extends TestCase{
      * @covers Pager\Pagination::postLinks
      * @covers Pager\Pagination::preLinks
      */
-    public function testCreatePager(){
+    public function testCreatePager()
+    {
         $pager = $this->pagination->setAClass('')->setLiClass('')->setPaginationClass('pagination')->paging(106, '/test-page');
         $this->assertStringStartsWith("<ul", $pager);
         $this->assertStringEndsWith("ul>", $pager);
-        $this->assertContains('<li class="active"><a href="/test-page?page=1" title="Page 1">1</a></li>', $pager);
+        $this->assertStringContainsString('<li class="active"><a href="/test-page?page=1" title="Page 1">1</a></li>', $pager);
         $this->assertEquals('<ul class="pagination"><li class="active"><a href="/test-page?page=1" title="Page 1">1</a></li><li><a href="/test-page?page=2" title="Page 2">2</a></li><li><a href="/test-page?page=3" title="Page 3">3</a></li><li><a href="/test-page?page=2" title="Page &gt;">&gt;</a></li><li><a href="/test-page?page=3" title="Page &raquo;">&raquo;</a></li></ul>', $pager);
         
         $alternateCurrent = $this->pagination->setAClass('')->setLiClass('')->setPaginationClass('pagination')->paging(106, '/test-page', 3);
         $this->assertStringStartsWith("<ul", $alternateCurrent);
         $this->assertStringEndsWith("ul>", $alternateCurrent);
-        $this->assertContains('<li class="active"><a href="/test-page?page=3" title="Page 3">3</a></li>', $alternateCurrent);
+        $this->assertStringContainsString('<li class="active"><a href="/test-page?page=3" title="Page 3">3</a></li>', $alternateCurrent);
         $this->assertEquals('<ul class="pagination"><li><a href="/test-page" title="Page &laquo;">&laquo;</a></li><li><a href="/test-page?page=2" title="Page &lt;">&lt;</a></li><li><a href="/test-page?page=1" title="Page 1">1</a></li><li><a href="/test-page?page=2" title="Page 2">2</a></li><li class="active"><a href="/test-page?page=3" title="Page 3">3</a></li></ul>', $alternateCurrent);
         
         $maxLinks = $this->pagination->paging(2506, '/test-page', 0, 50, 11, true);
-        $this->assertContains('Page 11', $maxLinks);
-        $this->assertNotContains('Page 12', $maxLinks);
+        $this->assertStringContainsString('Page 11', $maxLinks);
+        $this->assertStringNotContainsString('Page 12', $maxLinks);
         
         $maxLinksDiff = $this->pagination->paging(2506, '/test-page', 8, 50, 11, true);
-        $this->assertContains('Page 12', $maxLinksDiff);
-        $this->assertContains('&gt;', $maxLinksDiff);
-        $this->assertContains('&lt;', $maxLinksDiff);
-        $this->assertContains('&raquo;', $maxLinksDiff);
-        $this->assertContains('&laquo;', $maxLinksDiff);
+        $this->assertStringContainsString('Page 12', $maxLinksDiff);
+        $this->assertStringContainsString('&gt;', $maxLinksDiff);
+        $this->assertStringContainsString('&lt;', $maxLinksDiff);
+        $this->assertStringContainsString('&raquo;', $maxLinksDiff);
+        $this->assertStringContainsString('&laquo;', $maxLinksDiff);
     }
     
     /**
@@ -61,7 +65,8 @@ class PaginationTest extends TestCase{
      * @covers Pager\Pagination::postLinks
      * @covers Pager\Pagination::preLinks
      */
-    public function testNoPagerNeeded(){
+    public function testNoPagerNeeded()
+    {
         $this->assertFalse($this->pagination->paging(10, '/test-page'));
     }
     
@@ -75,14 +80,15 @@ class PaginationTest extends TestCase{
      * @covers Pager\Pagination::postLinks
      * @covers Pager\Pagination::preLinks
      */
-    public function testPagerArrows(){
+    public function testPagerArrows()
+    {
         $pager = $this->pagination->paging(320, '/test-page', 3, 50, 11, true);
-        $this->assertContains('&lt;', $pager);
-        $this->assertContains('&gt;', $pager);
+        $this->assertStringContainsString('&lt;', $pager);
+        $this->assertStringContainsString('&gt;', $pager);
         
         $pagerNoArrows = $this->pagination->paging(320, '/test-page', 3, 50, 11, false);
-        $this->assertNotContains('&lt;', $pagerNoArrows);
-        $this->assertNotContains('&gt;', $pagerNoArrows);
+        $this->assertStringNotContainsString('&lt;', $pagerNoArrows);
+        $this->assertStringNotContainsString('&gt;', $pagerNoArrows);
     }
     
     /**
@@ -95,15 +101,17 @@ class PaginationTest extends TestCase{
      * @covers Pager\Pagination::postLinks
      * @covers Pager\Pagination::preLinks
      */
-    public function testPagerWithQueryString(){        
-        $this->assertContains('/test-page?page=1&amp;search=dave&amp;age=45', $this->pagination->paging(100, '/test-page', 0, 50, 11, true, array('search' => 'dave', 'age' => '45')));
+    public function testPagerWithQueryString()
+    {
+        $this->assertStringContainsString('/test-page?page=1&amp;search=dave&amp;age=45', $this->pagination->paging(100, '/test-page', 0, 50, 11, true, array('search' => 'dave', 'age' => '45')));
     }
     
     /**
      * @covers Pager\Pagination::setPaginationClass
      * @covers Pager\Pagination::getPaginationClass
      */
-    public function testSetPagerClass(){
+    public function testSetPagerClass()
+    {
         $this->assertEquals('pagination justify-content-center', $this->pagination->getPaginationClass());
         $this->pagination->setPaginationClass('my-class');
         $this->assertNotEquals('pagination justify-content-center', $this->pagination->getPaginationClass());
@@ -116,7 +124,8 @@ class PaginationTest extends TestCase{
      * @covers Pager\Pagination::setLiActiveClass
      * @covers Pager\Pagination::getLiActiveClass
      */
-    public function testSetActiveClass(){
+    public function testSetActiveClass()
+    {
         $this->assertEquals('active', $this->pagination->getLiActiveClass());
         $this->pagination->setLiActiveClass('current');
         $this->assertNotEquals('active', $this->pagination->getLiActiveClass());
